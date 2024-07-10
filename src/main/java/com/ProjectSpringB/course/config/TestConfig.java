15cd3035e3af3ec33e82ef03d32288fdf19cdde0
Rafael Ferro <rafaelferromac@gmail.com>
2024-07-10 14:55:20 -0300

    Payment, one-to-one association

--- src/main/java/com/ProjectSpringB/course/config/TestConfig.java
+++ src/main/java/com/ProjectSpringB/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ProjectSpringB.course.entities.Category;
 import com.ProjectSpringB.course.entities.Order;
 import com.ProjectSpringB.course.entities.OrderItem;
+import com.ProjectSpringB.course.entities.Payment;
 import com.ProjectSpringB.course.entities.Product;
 import com.ProjectSpringB.course.entities.User;
 import com.ProjectSpringB.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z") , o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}//run
 	
